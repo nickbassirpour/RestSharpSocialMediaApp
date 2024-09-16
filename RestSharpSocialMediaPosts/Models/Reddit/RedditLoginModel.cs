1658c82bf84cedac67ae6bd177953cb61e85389a
nickbassirpour <nickbassirpour@gmail.com>
2024-09-15 20:31:00 -0700

    removing secret

--- RestSharpSocialMediaPosts/Models/Reddit/RedditLoginModel.cs
+++ RestSharpSocialMediaPosts/Models/Reddit/RedditLoginModel.cs
@@ -9,8 +9,8 @@ namespace RestSharpSocialMediaPosts.Models.Reddit
     public class RedditLoginModel
     {
 
-        public string ClientId { get; set; } = "F4jNvO2-A75lo2jxB08g7g";
-        public string ClientSecret { get; set; } = "03gFLr-6QnZl8fBZVW1pCczv1pZiyA";
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
         public string Username { get; set; } = "devtestercsharp";
         public string Password { get; set; } = "Come2MyGarden";
 
